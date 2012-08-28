d27d82f8e66684cfbc63ef4cbc2109a373320e4a
Elliot Neal <elliot@emdentec.com>
2012-08-28 11:27:38 +0100

    Add sample code for progress alert view

--- FSExtendedAlertKitExample/FSExtendedAlertKitExampleViewController.m
+++ FSExtendedAlertKitExample/FSExtendedAlertKitExampleViewController.m
@@ -76,7 +76,7 @@
 
 - (IBAction)showFSActivityIndicatorAlertButtonPressed:(id)sender {
     
-    FSActivityIndicatorAlertView *alert = [[FSActivityIndicatorAlertView alloc] initWithTitle:@"Title of the alert" message:nil];//]@"The message\nThe message\nThe message\nThe message"];
+    FSActivityIndicatorAlertView *alert = [[FSActivityIndicatorAlertView alloc] initWithTitle:@"Title of the alert" message:nil];//@"The message\nThe message\nThe message\nThe message"];
     
     [alert setCancelBlock:^ {
         NSLog(@"Cancelled.");
@@ -98,8 +98,60 @@
     
     double delayInSeconds = 2.0;
     dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, delayInSeconds * NSEC_PER_SEC);
-    dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
+    dispatch_after(popTime, dispatch_get_main_queue(), ^(void) {
         [alert dismiss:YES];
+    });
+}
+
+- (IBAction)showFSProgressAlertViewButtonPressed:(id)sender {
+    
+    FSProgressAlertView *alert = [[FSProgressAlertView alloc] initWithTitle:@"Title of the alert" message:nil];//@"The message\nThe message\nThe message\nThe message"];
+    [alert setCancelBlock:^ {
+        NSLog(@"Cancelled.");
+    }];
+    [alert setWillPresentBlock:^ {
+        NSLog(@"Will present.");
+    }];
+    [alert setDidPresentBlock:^ {
+        NSLog(@"Did present.");
+    }];
+    [alert setWillDismissBlock:^ {
+        NSLog(@"Will dismiss.");
+    }];
+    [alert setDidDismissBlock:^ {
+        NSLog(@"Did dismiss.");
+    }];
+    
+    [alert show];
+    
+    [alert setProgress:.1f animated:YES];
+    
+    double delayInSeconds = 2.0;
+    dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, delayInSeconds * NSEC_PER_SEC);
+    dispatch_after(popTime, dispatch_get_main_queue(), ^(void) {
+        
+        [alert setProgress:.5f animated:YES];
+        
+        double delayInSeconds = 1.5;
+        dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, delayInSeconds * NSEC_PER_SEC);
+        dispatch_after(popTime, dispatch_get_main_queue(), ^(void) {
+            
+            [alert setProgress:.75f animated:YES];
+            
+            double delayInSeconds = 1.0;
+            dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, delayInSeconds * NSEC_PER_SEC);
+            dispatch_after(popTime, dispatch_get_main_queue(), ^(void) {
+                
+                [alert setProgress:1.f animated:YES];
+                
+                double delayInSeconds = 0.5;
+                dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, delayInSeconds * NSEC_PER_SEC);
+                dispatch_after(popTime, dispatch_get_main_queue(), ^(void) {
+                    
+                    [alert dismiss:YES];
+                });
+            });
+        });
     });
 }
 
